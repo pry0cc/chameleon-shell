0798f9bdf5cade39c90efd642e79f1b3c7d5128e
pry0cc <pry0cc@protonmail.com>
2016-12-24 20:25:48 +0000

    Fixed run.sh script

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ echo "Removing existing container PHPshell. Press enter to continue, CTRL+C to e
 read
 sudo docker rm -f phpshell
 echo "Starting container with name phpshell"
-sudo docker run -d --name phpshell -v /var/www/:$(pwd) -p 80:80 tutum/lamp
+sudo docker run -d --name phpshell -v $(pwd):/app -p 80:80 tutum/lamp
 echo "Go to http://127.0.0.1/shell.php to test."
